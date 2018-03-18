7cfac0bb6f2b3f8c49f818f9d4cdc2a5360487a0
Krunal Yadav <krunal.yadav18@gmail.com>
2018-03-18 17:44:10 +0530

    Add production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,23 @@ const config = {
   resolve: {
     modules: [path.resolve('./lib'), path.resolve('./node_modules')]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: [ ],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
+
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }]
